76f67c32437c0de1652793ffe5e030a475ff251a
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-10 23:50:55 +0700

    section-9-295

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -0,0 +1,55 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    private String id;
+
+    private String title;
+
+    private Instructor instructor;
+
+    public Course() {
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
