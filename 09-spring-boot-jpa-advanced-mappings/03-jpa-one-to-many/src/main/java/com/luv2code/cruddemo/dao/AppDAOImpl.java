918b7c2dd95758155b8454b56a8f77c86112c9a3
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-20 18:29:19 +0700

    section-9-304

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -1,11 +1,15 @@
 package com.luv2code.cruddemo.dao;
 
+import com.luv2code.cruddemo.entity.Course;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class AppDAOImpl implements AppDAO {
@@ -53,6 +57,20 @@ public class AppDAOImpl implements AppDAO {
          tempInstructorDetail.getInstructor().setInstructorDetail(null);
 
          entityManager.remove(tempInstructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+        // create query
+        TypedQuery<Course> query = entityManager.createQuery(
+                "from Course  where instructor.id = :data", Course.class
+        );
+        query.setParameter("data", theId);
+
+        List<Course> courses = query.getResultList();
+
+        return courses;
+
     }
 
 
