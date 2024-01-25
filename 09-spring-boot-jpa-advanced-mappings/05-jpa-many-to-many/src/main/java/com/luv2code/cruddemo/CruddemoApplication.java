b8a530ce1ef9be991b33687e15f73f96c0850a74
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-26 03:19:38 +0700

    section-9-329

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -24,8 +24,23 @@ public class CruddemoApplication {
 		return runner -> {
 //			createCourseAndStudent(appDAO);
 
-			findCourseAndStudents(appDAO);
+//			findCourseAndStudents(appDAO);
+
+			findStudentAndCourses(appDAO);
 		};
+
+	}
+
+	private void findStudentAndCourses(AppDAO appDAO) {
+
+		int theId = 2;
+
+		Student tempStudent = appDAO.findStudentAndCoursesByStudentId(theId);
+
+		System.out.println("Loaded student: " + tempStudent);
+		System.out.println("Course: " + tempStudent.getCourses());
+
+		System.out.println("Done!");
 	}
 
 	private void findCourseAndStudents(AppDAO appDAO) {
