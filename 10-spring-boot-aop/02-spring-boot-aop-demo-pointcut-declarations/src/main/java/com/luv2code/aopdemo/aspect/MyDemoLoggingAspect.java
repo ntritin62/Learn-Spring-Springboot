4905f6cccb4487109c0f4680cb9ca1e8925ed128
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-29 20:43:30 +0700

    section-10-366

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,8 +1,10 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +13,16 @@ import org.springframework.stereotype.Component;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-
-
-
-
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJointPoint) {
 
         System.out.println("\n=====>>>> Executing @Before advice on method");
+
+        //display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJointPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+        //display method arguments
     }
 
 
