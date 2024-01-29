e97b352aeb969bdf8531d2912d5c7ccf05699b60
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-29 20:24:11 +0700

    section-10-363

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -1,4 +1,11 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.annotation.Before;
+
 public class MyCloudLogAsyncAspect {
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void logToCloudAsync() {
+        System.out.println("\n=====>>>> Logging to Cloud in async fashion");
+    }
 }
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,16 +11,7 @@ public class MyDemoLoggingAspect {
 
 
 
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-    private void forDaoPackage() {}
 
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-    private void getter() {}
-    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-    private void setter() {}
-
-    @Pointcut("forDaoPackage() && !(getter() || setter())")
-    private void forDaoPackageNoGetterSetter() {}
 
     @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
@@ -28,13 +19,6 @@ public class MyDemoLoggingAspect {
         System.out.println("\n=====>>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics() {
-        System.out.println("\n=====>>>> Performing API analytics");
-    }
 
-    @Before("forDaoPackageNoGetterSetter()")
-    public void logToCloudAsync() {
-        System.out.println("\n=====>>>> Logging to Cloud in async fashion");
-    }
+
 }
