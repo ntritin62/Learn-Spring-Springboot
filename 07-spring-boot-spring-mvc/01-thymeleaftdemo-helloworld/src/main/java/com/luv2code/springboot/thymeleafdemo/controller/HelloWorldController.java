3ec251ff676d4d3c7fcc0a64e4c966c10e43c2ed
ntritin62 <21522677@gm.uit.edu.vn>
2024-02-05 23:40:38 +0700

    section-6-173

--- 07-spring-boot-spring-mvc/01-thymeleaftdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleaftdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,17 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionthree(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey My Friend " + theName;
 
         model.addAttribute("message", result);
         return "helloworld";
