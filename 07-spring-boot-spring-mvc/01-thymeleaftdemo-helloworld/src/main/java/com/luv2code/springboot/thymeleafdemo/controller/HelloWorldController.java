be62d299f9185f4f3175b533b3b33f5d0acad95c
ntritin62 <21522677@gm.uit.edu.vn>
2024-02-05 18:05:17 +0700

    section-6-170

--- 07-spring-boot-spring-mvc/01-thymeleaftdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleaftdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +17,19 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+
+        String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
         return "helloworld";
     }
 }
