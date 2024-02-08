ad205ba359bb97e7da85f5c51870c99ce09e7046
ntritin62 <21522677@gm.uit.edu.vn>
2024-02-08 20:42:45 +0700

    section-6-203

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -15,6 +12,18 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to zero")
     private int freePasses;
+
+
+    @Pattern(regexp = "^[a-zA-z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
