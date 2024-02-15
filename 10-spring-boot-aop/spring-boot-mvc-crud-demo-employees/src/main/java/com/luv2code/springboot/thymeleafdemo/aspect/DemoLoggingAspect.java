d865a9ba42a91120c333925e843cfe94c191bb14
ntritin62 <21522677@gm.uit.edu.vn>
2024-02-15 17:54:35 +0700

    section-10-392

---
+++ 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+}
